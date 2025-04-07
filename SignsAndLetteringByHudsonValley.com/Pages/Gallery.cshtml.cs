2b93b5bd0142e71cdd1fbf707b977f42af0597a4
Javin Blanchard <jblanchard@mezrahconsulting.com>
2025-04-07 11:13:52 -0400

    Add project files.

---
+++ SignsAndLetteringByHudsonValley.com/Pages/Gallery.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SignsAndLetteringByHudsonValley.Pages
+{
+    public class GalleryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ SignsAndLetteringByHudsonValley.com/Pages/Services.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SignsAndLetteringByHudsonValley.Pages
+{
+    public class ServicesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
