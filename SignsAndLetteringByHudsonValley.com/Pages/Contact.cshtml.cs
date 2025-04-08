4be8e4ed8b262b1c213038e8dec44fd68a9e3743
Javin Blanchard <jblanchard@mezrahconsulting.com>
2025-04-08 10:21:23 -0400

    Added a site template

--- SignsAndLetteringByHudsonValley.com/Pages/Contact.cshtml.cs
+++ SignsAndLetteringByHudsonValley.com/Pages/Contact.cshtml.cs
@@ -1,12 +1,48 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace SignsAndLetteringByHudsonValley.Pages
 {
     public class ContactModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        [Required(ErrorMessage = "Please enter your name.")]
+        public string Name { get; set; }
+
+        [BindProperty]
+        [Required(ErrorMessage = "Please enter a valid email.")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [BindProperty]
+        [Phone(ErrorMessage = "Please enter a valid phone number.")]
+        public string Phone { get; set; }
+
+        [BindProperty]
+        [Required(ErrorMessage = "Please enter a message.")]
+        public string Message { get; set; }
+
+        public bool IsSubmitted { get; set; }
+
+        public void OnGet() { }
+
+        public IActionResult OnPost()
         {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // Process the form: save to DB, send email, etc.
+            IsSubmitted = true;
+
+            // Clear the fields (optional)
+            ModelState.Clear();
+
+            return Page();
         }
     }
 }
+
+
